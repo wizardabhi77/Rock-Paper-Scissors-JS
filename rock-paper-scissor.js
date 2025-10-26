43faa065ab0bfd713bf5095aeb360c6d12adbc61
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-10-26 18:08:53 +0530

    add function playRound to javascript
    Add if statements to check who won with computerChoice and humanChoice parameters

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,3 +1,6 @@
+const humanScore = 0;
+const computerScore = 0;
+
 function getComputerChoice(){
     let choice = ['rock', 'paper', 'scissors'];
     return choice[Math.floor(Math.random() * 3)];
@@ -6,6 +9,23 @@ function getComputerChoice(){
 function getHumanChoice(){
     const userInput = window.prompt('Enter Your Choice:','Rock, Paper, Scissor');
     return userInput;
+}
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice.toLowerCase === 'rock' && computerChoice === 'scissors' || 
+        humanChoice.toLowerCase === 'paper' && computerChoice === 'rock' ||
+        humanChoice.toLowerCase === 'scissors' && computerChoice === 'paper'){
+            humanScore++;
+            return console.log(`You win! ${humanChoice.toLowerCase} beats ${computerChoice}`);
+        }
+    else if(humanChoice.toLowerCase === computerChoice){
+        return console.log('You Draw!');
+    }
+    else {
+        computerScore++;
+        return console.log(`You Lose!,${computerChoice} beats ${humanChoice.toLowerCase}`);
+    }
+     
 }
 
 console.log('Hello World!');
